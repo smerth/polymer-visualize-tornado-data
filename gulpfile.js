3888c087b99f676b2c6bd6c70bf68e265b3eb042
Smerth <merth@me.com>
2018-11-13 14:59:38 -0800

    Updated readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require("gulp"),
-    deploy = require('gulp-gh-pages');
+  deploy = require("gulp-gh-pages");
 
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', function () {
-  return gulp.src("./build/bundled/**/*")
-    .pipe(deploy())
+gulp.task("deploy", function() {
+  return gulp.src("./build/default/**/*").pipe(deploy());
 });
